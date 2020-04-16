b72db4fb79d74110cc87d51b9334b0e1a0a68748
kuronekorangelf <60979615+kuronekorangelf@users.noreply.github.com>
2020-04-16 21:18:37 +0900

    武器マテリアルカラー変更
    武器のマテリアルカラーを残弾数に応じて緑から赤に変更する対応を行いました。

--- Assets/Script/Player/PlayerBase.cs
+++ Assets/Script/Player/PlayerBase.cs
@@ -4,37 +4,96 @@ using UnityEngine;
 
 public class PlayerBase : MonoBehaviour
 {
+	#region 定義
+
+	/// <summary>
+	/// 弾最大値
+	/// </summary>
+	[SerializeField]
+	private const uint BulletMax = 20;
+
+	/// <summary>
+	/// 発砲間隔（時間）
+	/// </summary>
+	[SerializeField]
+	private const float ShotTimeInterval = 0.2f;
+
+	/// <summary>
+	/// オーバーヒート待機時間（秒）
+	/// </summary>
+	[SerializeField]
+	private const float OverheatWaitTime = 3.0f;
+
+	/// <summary>
+	/// 装填までにかかる時間
+	/// 
+	[SerializeField]
+	private const float BulletChargeWaitTime = 1.0f;
+
+	/// <summary>
+	/// 装填間隔
+	/// </summary>
+	[SerializeField]
+	private const float BulletChargeInterval = 0.1f;
+
+	#endregion // 定義
+
 	#region フィールド
+
+	/// <summary>
+	/// camera
+	/// </summary>
+	[InspectorName("ゲームメインカメラ")]
+	public GameObject _PlayerCamera = null;
+
+	/// <summary>
+	/// 上下左右視点を切り替えると同時に回転させるオブジェクト
+	/// </summary>
+	private Transform _RotationObject = null;
+
+	/// <summary>
+	/// キャラクターコントローラー
+	/// </summary>
+	private CharacterController _CharacterController = null;
+
+	//----------------------------------------------------
+	// 移動管理
+	//----------------------------------------------------
 
 	/// <summary>
 	/// 視点移動スピード
 	/// </summary>
 	[InspectorName("視点移動スピード")]
-	public float _RotationSpeed = 2.0f;
+	[SerializeField]
+	private float _RotationSpeed = 2.0f;
 
 	/// <summary>
 	/// 移動量スピード
 	/// </summary>
 	[InspectorName("移動スピード")]
-	public float _MoveSpeed = 2.2f;
+	[SerializeField]
+	private float _MoveSpeed = 2.2f;
 
 	/// <summary>
 	/// ジャンプスピード
 	/// </summary>
 	[InspectorName("ジャンプスピード")]
-	public float _JumpSpeed = 5.0f;
+	[SerializeField]
+	private float _JumpSpeed = 5.0f;
 
 	/// <summary>
 	/// 重力
 	/// </summary>
 	[InspectorName("重力加速度")]
-	public float _Gravity = 0.05f;
+	[SerializeField]
+	private float _Gravity = 0.05f;
 
 	/// <summary>
 	/// 回転更新を行うか否か
 	/// </summary>
 	[InspectorName("回転更新を行うか否か")]
-	public bool _IsRotationUpdate = true;
+	[SerializeField]
+	private bool _IsRotationUpdate = true;
 
 	/// <summary>
 	/// 現在の回転
@@ -46,11 +105,53 @@ public class PlayerBase : MonoBehaviour
 	/// </summary>
 	private Vector3 _CurMoveDir = Vector3.zero;
 
-	private CharacterController _CharacterController = null;
+	//----------------------------------------------------
+	// 弾管理
+	//----------------------------------------------------
 
-	public GameObject _PlayerCamera = null;
+	/// <summary>
+	/// 残弾数と最大装填数の割合（残弾数/装填数）
+	/// </summary>
+	public float BulletNumRate
+	{
+		get
+		{
+			return (float)_BulletNum / (float)BulletMax;
+		}
+	}
 
-	private Transform _RotationObject = null;
+	/// <summary>
+	/// 現在の弾数
+	/// </summary>
+	[InspectorName("現在の弾数")]
+	[SerializeField]
+	private uint _BulletNum = BulletMax;
+
+	/// <summary>
+	/// 発砲間隔タイマー
+	/// </summary>
+	private float _ShotTimer = 0.0f;
+
+	/// <summary>
+	/// オーバーヒートタイマー
+	/// </summary>
+	private float _OverheatTimer = 0.0f;
+
+	/// <summary>
+	/// 装填待機タイマー
+	/// </summary>
+	private float _BulletChargeTimer = 0.0f;
+
+	/// <summary>
+	/// 装填間隔タイマー
+	/// </summary>
+	private float _BulletChargeIntervalTimer = 0.0f;
+
+	/// <summary>
+	/// 発砲更新処理管理
+	/// </summary>
+	private delegate void UpdateShotCallback();
+	private UpdateShotCallback _UpdateShotCallback = null;
 
 	#endregion // フィールド
 
@@ -62,6 +163,15 @@ public class PlayerBase : MonoBehaviour
 		_CharacterController = GetComponent<CharacterController>();
 
 		_RotationObject = gameObject.transform.Find("RotationObject");
+
+		// 初期化
+		_BulletNum = BulletMax;
+		_ShotTimer = 0.0f;
+		_OverheatTimer = 0.0f;
+		_BulletChargeTimer = 0.0f;
+		_BulletChargeIntervalTimer = 0.0f;
+
+		_UpdateShotCallback = updateShotIdleCallback;
 	}
 
 	/// <summary>
@@ -77,9 +187,16 @@ public class PlayerBase : MonoBehaviour
 
 		// 移動更新
 		updateMove();
+
+		// 弾
+		updateShot();
 	}
 
 	#region 非公開メソッド
+
+	//----------------------------------------------------
+	// camera管理
+	//----------------------------------------------------
 
 	/// <summary>
 	/// Rotaion更新処理
@@ -93,6 +210,10 @@ public class PlayerBase : MonoBehaviour
 		// Componentの追加されたGameObejctに対して回転
 		_RotationObject.rotation = Quaternion.Euler(_CurRotation.x, _CurRotation.y, _CurRotation.z);
 	}
+
+	//----------------------------------------------------
+	// Move管理
+	//----------------------------------------------------
 
 	/// <summary>
 	/// 移動更新処理
@@ -124,6 +245,112 @@ public class PlayerBase : MonoBehaviour
 
 		// 適用
 		_CharacterController.Move(_CurMoveDir * Time.deltaTime);
+	}
+
+	//----------------------------------------------------
+	// Shot管理
+	//----------------------------------------------------
+
+	/// <summary>
+	/// 発砲
+	/// </summary>
+	private void updateShot()
+	{
+		if (_UpdateShotCallback == null) 
+		{
+			Debug.Assert(false, "_UpdateShotCallback is null...");
+			return;
+		}
+
+		_UpdateShotCallback.Invoke();
+	}
+
+	/// <summary>
+	/// 発砲可能状態
+	/// </summary>
+	private void updateShotIdleCallback()
+	{
+		bool isInput = false;
+
+		// Shot
+		if (Input.GetMouseButton(0))
+		{
+			isInput = true;
+			_BulletChargeTimer = 0.0f;
+			_BulletChargeIntervalTimer = 0.0f;
+
+			_ShotTimer += Time.deltaTime;
+			if (_ShotTimer > ShotTimeInterval)
+			{
+				Debug.Log("ShotTest!BulletNum : " + _BulletNum.ToString());
+				_ShotTimer = 0.0f;
+
+				// 残弾を減らす
+				_BulletNum--;
+			}
+		}
+
+		// 残弾数無し
+		if(_BulletNum <= 0)
+		{
+			_ShotTimer = 0.0f;
+			_BulletChargeTimer = 0.0f;
+			_BulletChargeIntervalTimer = 0.0f;
+
+			// オーバーヒート
+			_UpdateShotCallback = updateShotOverheatCallback;
+			return;
+		}
+		// 装填
+		else if (_BulletNum < BulletMax)
+		{
+			// 入力なし
+			if (isInput == false)
+			{
+				// 装填までの待機タイマー
+				_BulletChargeTimer += Time.deltaTime;
+				if (_BulletChargeTimer > BulletChargeWaitTime)
+				{
+					// 装填
+					updateBulletCharge();
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// オーバーヒート状態
+	/// </summary>
+	private void updateShotOverheatCallback()
+	{
+		// オーバーヒート待機タイマー
+		_OverheatTimer += Time.deltaTime;
+		if (_OverheatTimer > OverheatWaitTime)
+		{
+			// 装填
+			updateBulletCharge();
+
+			// 装填完了したらIdle状態へ
+			if (_BulletNum >= BulletMax)
+			{
+				_OverheatTimer = 0.0f;
+				_UpdateShotCallback = updateShotIdleCallback;
+			}
+		}
+	}
+
+	/// <summary>
+	/// 装填
+	/// </summary>
+	private void updateBulletCharge()
+	{
+		// 装填間隔タイマー
+		_BulletChargeIntervalTimer += Time.deltaTime;
+		if (_BulletChargeIntervalTimer > BulletChargeInterval)
+		{
+			_BulletNum++;
+			_BulletChargeIntervalTimer = 0.0f;
+		}
 	}
 
 	#endregion
---
+++ Assets/Script/Player/WeaponControl.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponControl : MonoBehaviour
+{
+	#region 定義
+
+	/// <summary>
+	/// 燃料カラー情報変更用のRenderer格納クラス
+	/// </summary>
+	class FuelRendererData
+	{
+		public int _MaterialIndex;
+		public Renderer _Renderer;
+
+		public FuelRendererData(int index, Renderer r)
+		{
+			_MaterialIndex = index;
+			_Renderer = r;
+		}
+	}
+
+	/// <summary>
+	/// 燃料タンクオブジェクト最大数
+	/// </summary>
+	private static readonly uint FuelMax = 4;
+
+	/// <summary>
+	/// 燃料タンク１個分の割合
+	/// </summary>
+	private static readonly float FuelRateOnce = 1.0f / (float)FuelMax;
+
+	#endregion // 定義
+
+	#region フィールド
+
+	/// <summary>
+	/// 燃料タンクオブジェクトのリスト
+	/// </summary>
+	[InspectorName("燃料タンクオブジェクト格納")]
+	public GameObject[] _FuelObjArray;
+
+	/// <summary>
+	/// 燃料タンクが動く移動量
+	/// </summary>
+	[InspectorName("燃料タンクの移動量")]
+	[SerializeField]
+	private Vector3 _FuelObjMoveTarget = new Vector3(0, 0.2f, 0);
+
+	/// <summary>
+	/// カラー変更対象の燃料タンク用マテリアル
+	/// </summary>
+	[InspectorName("カラー変更対象の燃料タンク用マテリアル")]
+	[SerializeField]
+	private Material _FuelOverheatMaterial;
+
+	/// <summary>
+	/// カラー変更対象の燃料タンクのカラーグラデーション
+	/// </summary>
+	[InspectorName("燃料カラーグラデーション")]
+	[SerializeField, GradientUsage(true)]
+	private Gradient _FuelOverheatGradient;
+
+	/// <summary>
+	/// 燃料タンクオブジェクト初期位置
+	/// </summary>
+	private Vector3[] _FuelObjDefaultLocalPosition = new Vector3[FuelMax];
+
+	/// <summary>
+	/// 燃料タンクのRendererリスト
+	/// </summary>
+	private List<FuelRendererData> _FuelRenderList;
+
+	/// <summary>
+	/// 燃料タンクのカラー情報
+	/// </summary>
+	private MaterialPropertyBlock _FuelOverheatMaterialPropertyBlock;
+
+	/// <summary>
+	/// PlayerComponent
+	/// </summary>
+	private PlayerBase _PlayerBaseComponent;
+
+	#endregion // 定義
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		var length = _FuelObjArray.Length;
+		Debug.Assert(length > 0, "_FuelObjArray is none...");
+
+		for (var i = 0; i < length; i++)
+		{
+			// 初期位置
+			_FuelObjDefaultLocalPosition[i] = _FuelObjArray[i].transform.localPosition;
+		}
+
+		// Renderer
+		var renderers = GetComponentsInChildren<Renderer>(true);
+		if (renderers != null)
+		{
+			_FuelRenderList = new List<FuelRendererData>();
+			foreach (var renderer in renderers)
+			{
+				var materialLength = renderer.sharedMaterials.Length;
+				for (var i = 0; i < materialLength; i++)
+				{
+					if (renderer.sharedMaterials[i] == _FuelOverheatMaterial)
+					{
+						_FuelRenderList.Add(new FuelRendererData(i, renderer));
+						Debug.Log("FuelRendererList Add " + renderer.name);
+					}
+				}
+			}
+		}
+		_FuelOverheatMaterialPropertyBlock = new MaterialPropertyBlock();
+
+		_PlayerBaseComponent = gameObject.GetComponent<PlayerBase>();
+    }
+
+    // Update is called once per frame
+	void Update()
+	{
+		// 使用済み割合（1.0 - 残弾数 / 装填数）
+		var rate = 1.0f - _PlayerBaseComponent.BulletNumRate;
+
+		// 燃料位置アニメーション
+		updateFuelAnim(rate);
+
+		// 燃料カラーアニメーション
+		updateFuelColorAnim(rate);
+	}
+
+	/// <summary>
+	/// 残弾数に応じて燃料タンクのアニメーション制御(装填数MAXで1.0)
+	/// </summary>
+	/// <param name="rate"></param>
+	private void updateFuelAnim(float rate)
+	{
+		var length = _FuelObjArray.Length;
+		for (var i = 0; i < length; i++)
+		{
+			var r = 0.0f;
+			if (rate >= FuelRateOnce)
+			{
+				r = FuelRateOnce;
+			}
+			else
+			{
+				r = rate;
+			}
+
+			var obj = _FuelObjArray[i];
+
+			// アニメーション
+			var fuelRate = r / FuelRateOnce; // 1個単位の割合に変換
+			obj.transform.localPosition = _FuelObjDefaultLocalPosition[i] + _FuelObjMoveTarget * fuelRate;
+
+			rate -= r;
+		}
+	}
+
+	/// <summary>
+	/// 残弾数に応じて燃料タンクのカラー制御
+	/// </summary>
+	/// <param name="rate"></param>
+	private void updateFuelColorAnim(float rate)
+	{
+		foreach (var item in _FuelRenderList)
+		{
+			// 前設定情報を取得
+			item._Renderer.GetPropertyBlock(_FuelOverheatMaterialPropertyBlock);
+
+			// カラーを設定
+			_FuelOverheatMaterialPropertyBlock.SetColor("_EmissionColor", _FuelOverheatGradient.Evaluate(rate));
+
+			// RendererへPropertyBlockの設定
+			item._Renderer.SetPropertyBlock(_FuelOverheatMaterialPropertyBlock, item._MaterialIndex);
+		}
+	}
+}
